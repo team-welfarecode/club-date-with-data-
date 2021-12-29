2660396ad8d1e1838911af395d6db18abc556e77
nemo <galmae>
2021-12-30 05:40:18 +0900

    .

--- js/data.js
+++ js/data.js
@@ -2,46 +2,51 @@ const qnaList= [
     {
         q: '1번 질문',
         a: [
-            { answer: 'a. 1번 답변', type: [1] },
-            { answer: 'b. 2번 답변', type: [2] },
-            { answer: 'c. 3번 답변', type: [3] },
-            { answer: 'd. 4번 답변', type: [4] }
+            { answer: 'a. 1번 답변', type: 1 },
+            { answer: 'b. 2번 답변', type: 2 },
+            { answer: 'c. 3번 답변', type: 3 },
+            { answer: 'd. 4번 답변', type: 4 },
+            { answer: 'e. 5번 답변', type: 4 }
         ]
     },
     {
         q: '2번 질문',
         a: [
-            { answer: 'a. 1번 답변', type: [1] },
-            { answer: 'b. 2번 답변', type: [2] },
-            { answer: 'c. 3번 답변', type: [3] },
-            { answer: 'd. 4번 답변', type: [4] }
+            { answer: 'a. 1번 답변', type: 1 },
+            { answer: 'b. 2번 답변', type: 2 },
+            { answer: 'c. 3번 답변', type: 3 },
+            { answer: 'd. 4번 답변', type: 4 },
+            { answer: 'e. 5번 답변', type: 4 }
         ]
     },
     {
         q: '3번 질문',
         a: [
-            { answer: 'a. 1번 답변', type: [1] },
-            { answer: 'b. 2번 답변', type: [2] },
-            { answer: 'c. 3번 답변', type: [3] },
-            { answer: 'd. 4번 답변', type: [4] }
+            { answer: 'a. 1번 답변', type: 1 },
+            { answer: 'b. 2번 답변', type: 2 },
+            { answer: 'c. 3번 답변', type: 3 },
+            { answer: 'd. 4번 답변', type: 4 },
+            { answer: 'e. 5번 답변', type: 4 }
         ]
     },
     {
         q: '4번 질문',
         a: [
-            { answer: 'a. 1번 답변', type: [1] },
-            { answer: 'b. 2번 답변', type: [2] },
-            { answer: 'c. 3번 답변', type: [3] },
-            { answer: 'd. 4번 답변', type: [4] }
+            { answer: 'a. 1번 답변', type: 1 },
+            { answer: 'b. 2번 답변', type: 2 },
+            { answer: 'c. 3번 답변', type: 3 },
+            { answer: 'd. 4번 답변', type: 4 },
+            { answer: 'e. 5번 답변', type: 4 }
         ]
     },
     {
         q: '5번 질문',
         a: [
-            { answer: 'a. 1번 답변', type: [1] },
-            { answer: 'b. 2번 답변', type: [2] },
-            { answer: 'c. 3번 답변', type: [3] },
-            { answer: 'd. 4번 답변', type: [4] }
+            { answer: 'a. 1번 답변', type: 1 },
+            { answer: 'b. 2번 답변', type: 2 },
+            { answer: 'c. 3번 답변', type: 3 },
+            { answer: 'd. 4번 답변', type: 4 },
+            { answer: 'e. 5번 답변', type: 4 }
         ]
     },
 ]
--- js/start.js
+++ js/start.js
@@ -2,19 +2,35 @@ const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 const result = document.querySelector("#result");
 const endPoint = 5;
-const select = [0, 0, 0, 0, 0];
 
-function calResult() {
-    console.log(select);
-    var result = select.indexOf(Math.max(...select));
-    return result;
-}
+function goResult(score){
+    qna.style.WebkitAnimation = "fadeOut 0.5s";
+    qna.style.animation = "fadeOut 0.5s";
+    setTimeout(() => {
+      result.style.WebkitAnimation = "fadeIn 0.5s";
+      result.style.animation = "fadeIn 0.5s";
+      setTimeout(() => {
+        qna.style.display = "none";
+        result.style.display = "block";
+      }, 250);
+    setResult(score);
+    }, 250);
+  }
 
-function setResult() {
-    let point = calResult();
-    const resultName = document.querySelector('.resultname');
-    resultName.innerHTML = infoList[point].name;
-
+function setResult(score){
+    var point;
+    if(score>=90){
+        point=1;
+    }
+    else if(score>=60){
+        point=2;
+    }
+    else if(score>=40){
+        point=3;
+    }
+    else if(score>20){
+        point=4;
+    }
     var resultImg = document.createElement('img');
     const imgDiv = document.querySelector('#resultImg');
     var imgURL = 'img/image-' + point + '.png';
@@ -22,82 +38,64 @@ function setResult() {
     resultImg.alt = point;
     resultImg.classList.add('img-fluid');
     imgDiv.appendChild(resultImg);
-
-    const resultDesc = document.querySelector('.resultDesc');
-    resultDesc.innerHTML = infoList[point].desc;
+    document.getElementById("score").innerHTML=score;
 }
-
-function goResult() {
-    qna.style.WebkitAnimation = "fadeOut 0.5s";
-    qna.style.animation = "fadeOut 0.5s";
-    setTimeout(() => {
-        result.style.WebkitAnimation = "fadeIn 0.5s";
-        result.style.animation = "fadeIn 0.5s";
-        setTimeout(() => {
-            qna.style.display = "none";
-            result.style.display = "block";
-        }, 250);
-        setResult();
-    }, 250);
-}
-
-function addAnswer(answerText, qIdx, idx) {
+  
+  function addAnswer(answerText, qIdx, idx){
     var a = document.querySelector('.answerBox');
     var answer = document.createElement('button');
+    var score=0;
     answer.classList.add('answerList');
-    answer.classList.add('my-1');
-    answer.classList.add('py-2');
+    answer.classList.add('my-3');
+    answer.classList.add('py-3');
     answer.classList.add('mx-auto');
     answer.classList.add('fadeIn');
-
+  
     a.appendChild(answer);
     answer.innerHTML = answerText;
-
-    answer.addEventListener("click", function () {
-        var children = document.querySelectorAll('.answerList');
-        for (let i = 0; i < children.length; i++) {
-            children[i].disabled = true;
-            children[i].style.WebkitAnimation = "fadeOut 1s";
-            children[i].style.animation = "fadeOut 1s";
+  
+    answer.addEventListener("click", function(){
+      var children = document.querySelectorAll('.answerList');
+      for(let i = 0; i < children.length; i++){
+        children[i].disabled = true;
+        children[i].style.WebkitAnimation = "fadeOut 1s";
+        children[i].style.animation = "fadeOut 1s";
+      }
+      setTimeout(() => {
+        for(let i = 0; i < children.length; i++){
+          score += qnaList[qIdx].a[idx].type;
+          children[i].style.display = 'none';
         }
-        setTimeout(() => {
-            var target = qnaList[qIdx].a[idx].type;
-            for(let i = 0; i < target.length; i++){
-              select[target[i]] += 1;
-            }
-            for (let i = 0; i < children.length; i++) {
-                children[i].style.display = 'none';
-            }
-            goNext(++qIdx);
-        }, 50);
+        goNext(++qIdx, score);
+      },50)
     }, false);
-}
-
-function goNext(qIdx) {
-    if (qIdx === endPoint) {
-        goResult();
+  }
+  
+  function goNext(qIdx, score){
+    if(qIdx === endPoint){
+        goResult(score);
         return;
     }
     var q = document.querySelector('.qBox');
     q.innerHTML = qnaList[qIdx].q;
-    for (let i in qnaList[qIdx].a) {
-        addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
+    for(let i in qnaList[qIdx].a){
+      addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
     }
-    var status = document.querySelector('.statusBar')
-    status.style.width = (100 / endPoint) * (qIdx + 1) + '%';
-}
-
-function begin() {
+    var status = document.querySelector('.statusBar');
+    status.style.width = (100/endPoint) * (qIdx+1) + '%';
+  }
+  
+  function begin(){
     main.style.WebkitAnimation = "fadeOut 0.5s";
     main.style.animation = "fadeOut 0.5s";
     setTimeout(() => {
-        qna.style.WebkitAnimation = "fadeIn 0.5s";
-        qna.style.animation = "fadeIn 0.5s";
-        setTimeout(() => {
-            main.style.display = "none";
-            qna.style.display = "block";
-        }, 250);
-        let qIdx = 0;
-        goNext(qIdx);
+      qna.style.WebkitAnimation = "fadeIn 0.5s";
+      qna.style.animation = "fadeIn 0.5s";
+      setTimeout(() => {
+        main.style.display = "none";
+        qna.style.display = "block"
+      }, 250)
+      let qIdx = 0;
+      goNext(qIdx);
     }, 250);
-}
+  }
