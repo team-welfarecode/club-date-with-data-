9d6de5190fc5c546fc0f7fcbb4035658ad13ac65
nemo <galmae>
2021-12-30 04:18:45 +0900

    .

---
+++ js/data.js
@@ -0,0 +1,47 @@
+const qnaList= [
+    {
+        q: '1번 질문',
+        a: [
+            { answer: 'a. 1번 답변', type: [1] },
+            { answer: 'b. 2번 답변', type: [2] },
+            { answer: 'c. 3번 답변', type: [3] },
+            { answer: 'd. 4번 답변', type: [4] }
+        ]
+    },
+    {
+        q: '2번 질문',
+        a: [
+            { answer: 'a. 1번 답변', type: [1] },
+            { answer: 'b. 2번 답변', type: [2] },
+            { answer: 'c. 3번 답변', type: [3] },
+            { answer: 'd. 4번 답변', type: [4] }
+        ]
+    },
+    {
+        q: '3번 질문',
+        a: [
+            { answer: 'a. 1번 답변', type: [1] },
+            { answer: 'b. 2번 답변', type: [2] },
+            { answer: 'c. 3번 답변', type: [3] },
+            { answer: 'd. 4번 답변', type: [4] }
+        ]
+    },
+    {
+        q: '4번 질문',
+        a: [
+            { answer: 'a. 1번 답변', type: [1] },
+            { answer: 'b. 2번 답변', type: [2] },
+            { answer: 'c. 3번 답변', type: [3] },
+            { answer: 'd. 4번 답변', type: [4] }
+        ]
+    },
+    {
+        q: '5번 질문',
+        a: [
+            { answer: 'a. 1번 답변', type: [1] },
+            { answer: 'b. 2번 답변', type: [2] },
+            { answer: 'c. 3번 답변', type: [3] },
+            { answer: 'd. 4번 답변', type: [4] }
+        ]
+    },
+]
